048403d403142295a88181fe65fe71ec741b09d3
前島恵 <keimaejima@maejimameguminomacbook-pro.local>
2017-01-21 11:20:13 +0900

    Initial Commit

---
+++ creep_ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  creep_ios
+//
+//  Created by 前島恵 on 2017/01/21.
+//  Copyright © 2017年 keimaejima. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
